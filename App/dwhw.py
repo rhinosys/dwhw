c1ac3c1896b2e374ee8c872479e2655140662525
Nicolas Rineau <nicolas.rineau@rhinosys.fr>
2018-06-07 17:23:56 +0000

    add file

--- App/dwhw.py
+++ App/dwhw.py
@@ -1,2 +1,8 @@
-kldsmlfdsm
+# https://openweathermap.org/appid
+# Key: f89d7df890fc845b07197fc7d33d453e
+
+#http://api.openweathermap.org/data/2.5/forecast?id=2968254&APPID=f89d7df890fc845b07197fc7d33d453e&units=metric
+#296825 = Villeurbanne
+#APPID=Token
+
 
